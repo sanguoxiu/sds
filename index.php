b930db258e287277c698ae5f2acacdfcfb1a3f52
sanguoxiu <admin@sanguoxiu.com>
2013-12-26 12:41:48 +0800

    Revert "第一版本"
    This reverts commit fce63730098544e26eb8ed046b08e7701e3ad8fd.

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// $title = "首页";
-require_once './source/common.inc.php';
-require_once SGS_ROOT . './template/default/index.php';
-?>
--- source/js/ie6_tips.js
+++
@@ -1,7 +0,0 @@
-var ie6_tips = '';
-ie6_tips += '<div id="ie6-warning" style="width:100%;position:absolute;top:0;left:0;background:#ffffe1;padding:5px 0;font-size:12px">';
-ie6_tips += '<p style="width:900px;margin:0 auto;">您正在使用 Internet Explorer 6.0，在本页面的显示效果可能有差异。建议您升级到 <a href="http://www.microsoft.com/china/windows/internet-explorer/" target="_blank">Internet Explorer 8</a>，';
-ie6_tips += '或使用以下浏览器：';
-ie6_tips += '<a href="http://www.mozillaonline.com/">Firefox</a> / <a href="http://www.google.com/chrome/?hl=zh-CN">Chrome</a> / <a href="http://www.apple.com.cn/safari/">Safari</a> / <a href="http://www.operachina.com/">Opera</a>';
-ie6_tips += '</p></div>';
-document.write (ie6_tips);
